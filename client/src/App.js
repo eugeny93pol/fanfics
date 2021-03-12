c7bd03045e21c9afc66fcf1d7effc07dadd5b9eb
Евгений Полуянович <eugeny.pol93@gmail.com>
2021-03-12 21:36:59 +0300

    before eslint installation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ThemeContext } from './context/ThemeContext'
-import { LanguageContext } from './context/LanguageContext'
 import { AuthContext } from './context/AuthContext'
 import { useTheme } from './hooks/theme.hook'
 import { useAuth } from './hooks/auth.hook'
 import { useRoutes } from './routes/routes'
 import { Navbar } from './components/Navbar'
 import { Loader } from './components/Loader'
-//import { useLanguage } from './hooks/language.hook'
+import Modal from './components/modal'
 import './i18n/i18n'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
@@ -16,7 +15,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min'
 function App() {
     const { token, login, logout, userData, isAuth, ready } = useAuth()
     const { theme, toggleTheme } = useTheme()
-    //const { language, toggleLanguage } = useLanguage()
     const routes = useRoutes(isAuth)
 
     const loader = <Loader classes={['my-5']}/>
@@ -27,18 +25,14 @@ function App() {
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            {/*<LanguageContext.Provider value={{ language, toggleLanguage }}>*/}
-                <AuthContext.Provider value={{ token, login, logout, userData, isAuth }}>
-                    <Router>
-                        <Suspense fallback={ loader }>
-                            <Navbar/>
-                            <div className="container-lg">
-                                { routes }
-                            </div>
-                        </Suspense>
-                    </Router>
-                </AuthContext.Provider>
-            {/*</LanguageContext.Provider>*/}
+            <AuthContext.Provider value={{ token, login, logout, userData, isAuth }}>
+                <Router>
+                    <Suspense fallback={ loader }>
+                        <Navbar/>
+                        <div className="container-lg">{ routes }</div>
+                    </Suspense>
+                </Router>
+            </AuthContext.Provider>
         </ThemeContext.Provider>
     )
 }
---
+++ client/src/components/modal/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const Modal = () => {
+    return(
+        <h1>modal message</h1>
+    )
+}
+export default Modal
