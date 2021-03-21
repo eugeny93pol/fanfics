84a7401c6b2b7da61d618a038a9f50e4b423a3b3
Евгений Полуянович <eugeny.pol93@gmail.com>
2021-03-22 00:55:29 +0300

    comments

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -1,10 +1,8 @@
 const router = require('express').Router()
 const role = require('../middleware/role.middleware')
 const { postComment } = require('../controllers/comment.controller')
-//const { getComments } = require('../controllers/comment.controller')
 
 
-//router.get('/', role(['admin', 'user']), getComments)
 router.post('/', role(['admin', 'user']), postComment)
 
 module.exports = router
