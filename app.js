fef419c442a13385a09692cca383801f1bfd6836
Евгений Полуянович <eugeny.pol93@gmail.com>
2021-03-26 01:25:29 +0300

    wtf1

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get("/auth/facebook", passport.authenticate("facebook"));
 app.get("/auth/facebook/callback",
     passport.authenticate("facebook"),
     (req, res) => {
-        res.redirect("/profile");
+        res.redirect("/");
     });
 //-->
 
